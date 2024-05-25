dd0dde6071a0b9ce4624df83eccafd40c8ceb9dd
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-05-25 18:38:15 -0300

    feriados

--- middlewares/isOpenMiddleware.js
+++ middlewares/isOpenMiddleware.js
@@ -16,34 +16,44 @@ export const isOpenMiddleware = (req, res, next) => {
 	const now = moment().tz("America/Argentina/Buenos_Aires");
 	const day = now.day(); // Domingo = 0, Lunes = 1, ..., Sábado = 6
 	const hour = now.hours();
-    console.log("now:", now)
-    console.log("day:", day)
-    console.log("hour:", hour)
-    console.log("name:", name)
+	const currentDate = now.format("MM-DD"); // Formato mes-día
+	console.log("now:", now);
+	console.log("day:", day);
+	console.log("hour:", hour);
+	console.log("name:", name);
 
 	const isWeekday = day >= 1 && day <= 5; // Lunes a Viernes
 	const isSaturday = day === 6; // Sábado
 	const isSunday = day === 0; // Domingo
-    console.log("isSaturday:", isSaturday)
+	console.log("isSaturday:", isSaturday);
+
+	const holidays = process.env.HOLIDAYS.split(",");
 
 	const weekdayStartHour = parseInt(process.env.WEEKDAY_START_HOUR, 10);
 	const weekdayEndHour = parseInt(process.env.WEEKDAY_END_HOUR, 10);
 	const weekendStartHour = parseInt(process.env.WEEKEND_START_HOUR, 10);
-    console.log("weekensStartHour:", weekendStartHour)
+	console.log("weekensStartHour:", weekendStartHour);
 
 	const isEveningOrNight = hour >= weekdayStartHour || hour < weekdayEndHour; // 7pm a 9am
+	const isHoliday = holidays.includes(currentDate);
+	console.log("isholiday", isHoliday)
+	
 	if (
 		(isWeekday && isEveningOrNight) ||
 		(isSaturday && hour >= weekendStartHour) ||
 		isSunday ||
 		(isWeekday && hour < weekdayEndHour) ||
 		name === "Gustavo Gomez Villafañe" ||
-		name === "Gg"
+		name === "Gg" ||
+		isHoliday
 	) {
 		next();
 	} else {
 		// Exit the process
-        console.log("Exiting the process according Megamoto opening schedule:", now )
+		console.log(
+			"Exiting the process according Megamoto opening schedule:",
+			now
+		);
 		res.status(200).send("MegaBot no está disponible en este horario.");
 		return;
 	}
