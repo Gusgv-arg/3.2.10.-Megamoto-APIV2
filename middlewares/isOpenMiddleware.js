c42d15897deedc4c4985f95e577a77f40d34611a
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-05-30 17:03:02 -0300

    agent

--- middlewares/isOpenMiddleware.js
+++ middlewares/isOpenMiddleware.js
@@ -13,31 +13,36 @@ export const isOpenMiddleware = (req, res, next) => {
 		? data.webUser
 		: "No name";
 
+	let agent = "";
+	if (data.interaction?.proactive === true) {
+		agent = true;
+	}
+	
 	const now = moment().tz("America/Argentina/Buenos_Aires");
 	const day = now.day(); // Domingo = 0, Lunes = 1, ..., Sábado = 6
 	const hour = now.hours();
 	const currentDate = now.format("MM-DD"); // Formato mes-día
-	console.log("now:", now);
+	/* console.log("now:", now);
 	console.log("day:", day);
 	console.log("hour:", hour);
-	console.log("name:", name);
+	console.log("name:", name); */
 
 	const isWeekday = day >= 1 && day <= 5; // Lunes a Viernes
 	const isSaturday = day === 6; // Sábado
 	const isSunday = day === 0; // Domingo
-	console.log("isSaturday:", isSaturday);
+	//console.log("isSaturday:", isSaturday);
 
 	const holidays = process.env.HOLIDAYS.split(",");
 
 	const weekdayStartHour = parseInt(process.env.WEEKDAY_START_HOUR, 10);
 	const weekdayEndHour = parseInt(process.env.WEEKDAY_END_HOUR, 10);
 	const weekendStartHour = parseInt(process.env.WEEKEND_START_HOUR, 10);
-	console.log("weekensStartHour:", weekendStartHour);
+	//console.log("weekensStartHour:", weekendStartHour);
 
 	const isEveningOrNight = hour >= weekdayStartHour || hour < weekdayEndHour; // 7pm a 9am
 	const isHoliday = holidays.includes(currentDate);
-	console.log("isholiday", isHoliday)
-	
+	//console.log("isholiday", isHoliday)
+
 	if (
 		(isWeekday && isEveningOrNight) ||
 		(isSaturday && hour >= weekendStartHour) ||
@@ -45,8 +50,9 @@ export const isOpenMiddleware = (req, res, next) => {
 		(isWeekday && hour < weekdayEndHour) ||
 		name === "Gustavo Gomez Villafañe" ||
 		name === "Gg" ||
-		isHoliday
+		isHoliday || agent === true
 	) {
+		console.log("Data continues. Agent:", agent)
 		next();
 	} else {
 		// Exit the process
